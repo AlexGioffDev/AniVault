a03fefbf3496635e42b2e2623ec80e471ebac9fb
razeft <alex.gioffre_dev@outlook.it>
2025-01-22 18:40:48 +0100

    Features: Services - Recommendations API

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,30 +5,41 @@ import { AnimeSearch } from '../models/AnimeSearch';
 import { AnimeData } from '../models/Anime';
 import { AnimeCharacters } from '../models/AnimeCharacters';
 import { AnimeReviews } from '../models/AnimeReviews';
+import { AnimeRecommendations } from '../models/AnimeRecommendations';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTopAnime(page: number): Observable<AnimeSearch> {
-    return this.http.get<AnimeSearch>(`https://api.jikan.moe/v4/top/anime?type=tv&order_by=score&page=${page}`)
+    return this.http.get<AnimeSearch>(
+      `https://api.jikan.moe/v4/top/anime?type=tv&order_by=score&page=${page}`
+    );
   }
 
-
   getAnime(id: number): Observable<AnimeData> {
-    return this.http.get<AnimeData>(`https://api.jikan.moe/v4/anime/${id}/full`)
+    return this.http.get<AnimeData>(
+      `https://api.jikan.moe/v4/anime/${id}/full`
+    );
   }
 
   getAnimeCharacters(id: number): Observable<AnimeCharacters> {
-    return this.http.get<AnimeCharacters>(`https://api.jikan.moe/v4/anime/${id}/characters`);
+    return this.http.get<AnimeCharacters>(
+      `https://api.jikan.moe/v4/anime/${id}/characters`
+    );
   }
-  
 
   getAnimeReviews(id: number): Observable<AnimeReviews> {
-    return this.http.get<AnimeReviews>(`https://api.jikan.moe/v4/anime/${id}/reviews`);
+    return this.http.get<AnimeReviews>(
+      `https://api.jikan.moe/v4/anime/${id}/reviews`
+    );
   }
-  
+
+  getAnimeRecommendations(id: number): Observable<AnimeRecommendations> {
+    return this.http.get<AnimeRecommendations>(
+      `https://api.jikan.moe/v4/anime/${id}/recommendations`
+    );
+  }
 }
