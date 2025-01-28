843083d8af29c65571b5ffad2bdabb0b67f95aa8
razeft <alex.gioffre_dev@outlook.it>
2025-01-28 16:32:19 +0100

    Install Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Scansiona tutti i file HTML e TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
